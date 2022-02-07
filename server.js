2896771af3fde3d77b0504015e70fc101fe17abe
Presa Theppanya <you@example.com>
2022-02-06 21:41:20 -0600

    activity-2

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 import express from 'express';
 
+// initialize express application
+const app = express();
+
+// API endpoints
+app.get('/', (req, res) =>
+    res.send('http get request sent to root api endpoint')
+);
+
+// connection listener
+app.listenerCount(3000, () => console.log('Express server running on port 3000'));
