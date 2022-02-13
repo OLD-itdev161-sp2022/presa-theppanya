8919868eb1a4e2932343e3b45cb23b8cd252b621
Presa Theppanya <you@example.com>
2022-02-13 13:43:48 -0600

    Added database connection

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import connectDatabase from './config/db';
 // initialize express application
 const app = express();
 
+// connect database
+connectDatabase();
+
 // API endpoints
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
